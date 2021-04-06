be41985ab93b1a6ef0bbaa75c43fd90a69124f3f
andrii-pulya <andrey.predein@gmail.com>
2021-04-06 16:44:59 +0300

    task 1 ready

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,6 @@ console.log(`В списке ${categories.length} категории.`);
 
 categories.forEach(category => {
     const title = category.querySelector('h2').textContent;
-    console.log(title);
     const count = category.querySelectorAll('li').length;
     console.log(`Категория: ${title}`);
     console.log(`Количество элементов: ${count}`);
